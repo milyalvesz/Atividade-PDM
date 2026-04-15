ec46b8c3d000ddb4c5a92aeb02f34df5d50e5156
Jamily Emily Alves Barbosa <jamily.emily@academico.ifpb.edu.br>
2026-04-15 11:04:56 -0300

    Update image and site information in teste.py

--- teste.py
+++ teste.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.image("teste.png")
-st.write("Site Dinaldo Jorge")
-st.link_button("Acessar", "https://www.dinaldo.com.br")
+st.image("mimiu.jpeg")
+st.write("Site Jamily Alves")
+st.link_button("Acessar", "https://sites.google.com/academico.ifpb.edu.br/jamily/in%C3%ADcio")
