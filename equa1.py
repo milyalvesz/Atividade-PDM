f5e4169ef0349b63102ef2ec0dc643581a7c9a7f
Jamily Emily Alves Barbosa <jamily.emily@academico.ifpb.edu.br>
2026-08-31 08:03:55 -0300

    Add files via upload

--- equa1.py
+++ equa1.py
@@ -21,7 +21,7 @@ PASTA_APP = Path(__file__).parent
 # ============================================
 # CAMINHO DA LOGOMARCA
 # ============================================
-CAMINHO_LOGO = PASTA_APP / "mimiu.jpeg"
+CAMINHO_LOGO = PASTA_APP / "mat.jpeg"
 
 # ============================================
 # LOGOMARCA
